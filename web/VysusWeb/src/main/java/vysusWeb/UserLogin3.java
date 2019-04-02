fa2bba42977472bfeb3f6a4e35a6eeae7242f3ba
Miles Everett <miles_everett@hotmail.co.uk>
2019-04-02 14:01:05 +0100

    Created new class with base functionality

---
+++ web/VysusWeb/src/main/java/vysusWeb/UserLogin3.java
@@ -0,0 +1,52 @@
+package vysusWeb;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.HashMap;
+
+import javax.faces.bean.ManagedBean; 
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import request.*;
+import storage.*;
+
+@ManagedBean(name="login")
+@SessionScoped
+
+public class UserLogin3 implements Serializable{
+	
+	String username;
+	String password;
+	
+	public UserLogin3 () {
+		
+	}
+	
+	public String loginUser() {
+		return "index";
+	}
+		
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}

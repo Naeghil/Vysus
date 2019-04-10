4ba71d73b4546834c36ca05d0331327d2917d4bb
Miles Everett <miles_everett@hotmail.co.uk>
2019-04-10 17:13:45 +0100

    More tests

--- web/VysusWeb/src/main/java/vysusWeb/UserLogin.java
+++ web/VysusWeb/src/main/java/vysusWeb/UserLogin.java
@@ -62,7 +62,7 @@ public class UserLogin implements Serializable {
 			Map<String, Object> requestMap = context.getExternalContext().getSessionMap();
 			UIViewRoot testData3 = context.getViewRoot();
 			UIComponent testData2 = context.getViewRoot().findComponent("password");
-			Map<String,Object> testData = context.getViewRoot().findComponent("inputSecret").getAttributes();
+			Map<String,Object> testData = context.getViewRoot().findComponent("username").getAttributes();
 			System.out.println(testData);
 			
 			System.out.println(testData2);

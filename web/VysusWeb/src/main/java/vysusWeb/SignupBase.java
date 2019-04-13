a32437408a2e278bd3abd9eb9e5cae431fbdb919
Miles Everett <miles_everett@hotmail.co.uk>
2019-04-13 14:32:32 +0100

    Username exception fix

--- web/VysusWeb/src/main/java/vysusWeb/SignupBase.java
+++ web/VysusWeb/src/main/java/vysusWeb/SignupBase.java
@@ -52,7 +52,7 @@ public class SignupBase extends VysusBean {
 			String password = (String)userData.get("password");
 			String accountID = (Integer)accountData.get("accType")+username;
 			System.out.println("Acc data created");
-			if(User.exists(username, connection)) throw new InvalidDataException("username", "This username already exists.");
+			if(User.exists(username, connection)) throw new InvalidDataException(null, "This username already exists.");
 			System.out.println(username);
 			System.out.println(accountID);
 			
